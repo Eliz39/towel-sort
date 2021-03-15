85a35b3c701631300b3b262ad5b2ca8d02bc0635
Eliz39 <liza.nikitina9898@gmail.com>
2021-03-16 00:57:34 +0100

    feat: change solution

--- src/index.js
+++ src/index.js
@@ -6,38 +6,26 @@ module.exports = function towelSort (matrix) {
     if (matrix === undefined || matrix.length === 0) { return []}
     else {
       var max = matrix[0].length -1;
-
-    // Grab the first row | result.push.apply(result,array[0])
     for (var i=0; i<=max; i++){
         result.push(matrix[0][i]);
     }
-
-    // Grab the last column
     for (var i=1; i< max; i++){
         result.push(matrix[i][max]);
     }
-    // Grab the last row
     for (var i=max; i>=0; i--){
         result.push(matrix[max][i]);
     }
-
-    // Grab the first column
     for (var  i=max-1; i> 0; i--){
         result.push(matrix[i][0]);
     }
-
     subarray = [];
-    // Form the inner matrix
-    for (var i=1; i<max; i++){
-        subarray.push(matrix[i].splice(1,max-1));
+    for (var i=1; i < max; i++){
+        subarray.push(matrix[i].splice(1, max-1));
     }
-
-    //call it recursively
     result = result.concat( towelSort(subarray) );
-
-
+    return result;
 }
-return result;
+
 }
 
 
