9d717bece6ee28e1ce40df79a96a96a6c3374c6c
Eliz39 <liza.nikitina9898@gmail.com>
2021-03-15 23:55:08 +0100

    feat: add part of solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (matrix === undefined || matrix.length === 0) { return []}
+    else {
+      for ( let i = 1; i < matrix.length; i+2) {
+            let newArr =  matrix[i]
+            let reverseMatrix = newArr.reverse()
+            matrix.splice(i, 1, reverseMatrix )
+          }
+        var merged = [].concat.apply([], matrix);
+
+    }
+    return merged
+    }
+
+
+
